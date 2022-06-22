6c317b8ea8187a37b28fb76fe3d51be3474b45a5
Kalka <k@kalka.io>
2022-06-22 06:06:34 -0400

    remove some testing stuff

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,10 +3,6 @@ import { config, library } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
-import LogRocket from "logrocket";
-LogRocket.init("nnfytt/test");
-console.log("this ran");
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
